a248319d4e51d6946016709a1f46dee0544b5638
Rutaihwa <rutaihwa@gmail.com>
2019-10-26 15:00:36 +0200

    Simple begining

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
 fn main() {
-    println!("Hello, world!");
+    // Server address
+    let addr = ([127,0,0,1], 8080).into();
+    
+    // Server instance
+    let builder = Server::bind(&addr);
+
+    let server = builder.serve(|| {
+        service_fn_ok(|_| {
+            Response::new(Body::from("This is where it begins"))
+        })
+    });
+
+    let server = server.map_err(drop);
+
+    hyper::rt::run(server);
 }
