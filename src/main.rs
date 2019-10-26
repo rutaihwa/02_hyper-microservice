52967ee4d93dc74c2a0676bc36e7afc9da35cc65
Rutaihwa <rutaihwa@gmail.com>
2019-10-26 18:55:21 +0200

    Simple index

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
-use hyper::{Body, Response, Server};
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::{Body, Request, Response, Server, Error, Method, StatusCode};
+use futures::{Future, future};
+use hyper::service::service_fn;
 
 fn main() {
     // Server address
     let addr = ([127,0,0,1], 8080).into();
-    
+
     // Server instance
     let builder = Server::bind(&addr);
 
-    let server = builder.serve(|| {
-        service_fn_ok(|_| {
-            Response::new(Body::from("This is where it begins"))
-        })
-    });
+    // Building a server
+    let server = builder.serve(|| service_fn(
+        microservice_handler
+    ));
 
+    // Dealing with errors
     let server = server.map_err(drop);
 
+    // Running the server
     hyper::rt::run(server);
 }
+
+// Microservice hander
+fn microservice_handler (req: Request<Body>) ->
+    impl Future<Item = Response<Body>, Error=Error> {
+        {
+            match(req.method(), req.uri().path()) {
+                (&Method::GET, "/") => {
+                    future::ok(
+                        Response::new(INDEX.into())
+                    )
+                },
+                _ => {
+                    let response = Response::builder()
+                        .status(StatusCode::NOT_FOUND)
+                        .body(Body::empty())
+                        .unwrap();
+
+                    future::ok(response)
+                },
+            }
+        }
+    }
+
+// Index
+const INDEX: &'static str = r#"
+<!doctype html>
+<html>
+  <head>
+    <title>02: Hyper microservice</title>
+  </head>
+  <body>
+    <h3>This is where it all starts.</h3>
+  </body>
+</html>
+"#;
